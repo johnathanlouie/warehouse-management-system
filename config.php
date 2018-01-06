66b1467d2c801dd1f53c8a127b22ac365559385e
Jonathan Louie <johnlouie88@outlook.com>
2018-01-05 17:30:26 -0800

    Changed MySQL connector.

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+const DB_HOST = 'localhost';
+const DB_PORT = 3306;
+const DB_USER = 'root';
+const DB_PASSWORD = '';
+const DB_NAME = 'wms';
+const DB_CHARSET = 'utf8mb4';
---
+++ mysql.php
@@ -0,0 +1,89 @@
+<?php
+
+require_once 'config.php';
+
+namespace wms;
+
+class MysqlConnectionException extends \Exception {
+    
+}
+
+class MysqlCharsetException extends \Exception {
+    
+}
+
+class MysqlParamException extends \Exception {
+    
+}
+
+class MysqlPreparationException extends \Exception {
+    
+}
+
+class MysqlQueryException extends \Exception {
+    
+}
+
+class MysqlGetResultException extends \Exception {
+    
+}
+
+class Mysql {
+
+    private $mysqli;
+
+    public function __construct() {
+        $this->mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT);
+        if ($this->mysqli->connect_error) {
+            throw new MysqlConnectionException($this->mysqli->connect_error, $this->mysqli->errno);
+        }
+        $this->mysqli->set_charset(DB_CHARSET);
+        if ($this->mysqli->connect_error) {
+            throw new MysqlCharsetException($this->mysqli->connect_error, $this->mysqli->errno);
+        }
+    }
+
+    public function preparedQuery($sql, $types, $params) {
+        $stmt = $this->mysqli->prepare($sql);
+        if (!$stmt) {
+            throw new MysqlPreparationException($this->mysqli->connect_error, $this->mysqli->errno);
+        }
+        foreach ($params as $value) {
+            $bind = $stmt->bind_param($types, ...$value);
+            if (!$bind) {
+                throw new MysqlParamException($stmt->error, $stmt->errno);
+            }
+            $exe = $stmt->execute();
+            if (!$exe) {
+                throw new MysqlParamException($stmt->error, $stmt->errno);
+            }
+        }
+        $resultObj = $stmt->get_result();
+        if ($this->mysqli->errno) {
+            throw new MysqlGetResultException($stmt->error, $stmt->errno);
+        }
+        $results = null;
+        if ($resultObj) {
+            $results = $resultObj->fetch_all(MYSQLI_ASSOC);
+            $resultObj->free();
+        }
+        $stmt->reset();
+        $stmt->close();
+        return $results;
+    }
+
+    public function query($sql) {
+        $resultObj = $this->mysqli->query($sql);
+        if ($resultObj === false) {
+            throw new MysqlQueryException();
+        } elseif ($resultObj === true) {
+            return null;
+        }
+        return $resultObj->fetch_all(MYSQLI_ASSOC);
+    }
+
+    public function close() {
+        $this->mysqli->close();
+    }
+
+}
